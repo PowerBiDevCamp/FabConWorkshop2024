beda7dc1c7467d7685bfefd94ef3b88b3b45f2f1
Ted Pattison <ted@tedpattison.net>
2024-03-17 08:34:11 -0400

    Updates

---
+++ Demo07-CreateShortcut/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string workspaceName = "Demo7 - CreateShortcut";
+
+//CustomerTenantBuilder.CreateCustomerTenantWithAdlsGen2Shortcut(workspaceName);
+
+
+CustomerTenantBuilder.CreateCustomerTenantWithWarehouse(workspaceName);
+
+
+//CustomerTenantBuilder.GetShortcutForLakehouse("Acme Corp", "Cutup");
+
+// Test1 
+// CustomerTenantBuilder.CreateCustomerTenant(workspaceName);
+
+// Test2
+//CustomerTenantBuilder.CreateShortcutsToStagingWorkspace(workspaceName);
---
+++ Demo8-CreateDataPipeline/Program.cs
@@ -0,0 +1,16 @@
+﻿
+string workspaceName = "Acme Corp";
+
+//CustomerTenantBuilder.CreateCustomerTenantWithDataPipeline(workspaceName);
+
+
+//CustomerTenantBuilder.CreateCustomerTenantWithWarehouse(workspaceName);
+
+
+CustomerTenantBuilder.CreateCustomerTenantWithDataPipeline(workspaceName);
+
+// Test1 
+// CustomerTenantBuilder.CreateCustomerTenant(workspaceName);
+
+// Test2
+//CustomerTenantBuilder.CreateShortcutsToStagingWorkspace(workspaceName);
